6d9b1893c23aec724236be6f4c6dc88e6f7aadb0
Vadis00 <vadym.gomeniuk@binary-studio.com>
2023-02-17 19:20:12 +0200

    Add project files.

---
+++ Cool data processing service/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+public class Program
+{
+    static void Main()
+    {
+        var watcher = new FileSystemWatcher();
+        watcher.Path = @"C:\Users\vgome\OneDrive\Рабочий стол\test";
+        watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+        watcher.Filter = "*.*";
+        watcher.Changed += OnChanged;
+        watcher.EnableRaisingEvents = true;
+
+        Console.ReadLine();
+    }
+
+    private static void OnChanged(object source, FileSystemEventArgs e)
+    {
+        Console.WriteLine("ff");
+        //Copies file to another directory.
+    }
+
+}
---
+++ Cool data processing service/Service/FileWatcherService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cool_data_processing_service.Service
+{
+    public class FileWatcherService
+    {
+        private readonly FileSystemWatcher fileSystemWatcher;
+        public FileWatcherService()
+        {
+         //   fileSystemWatcher = new();
+        }
+
+        public void ListenUpdate() => fileSystemWatcher.Changed += OnChanged;
+        
+        public void Stoplistening() => fileSystemWatcher.Changed -= OnChanged;
+
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine("ff");
+        }
+
+    }
+}
